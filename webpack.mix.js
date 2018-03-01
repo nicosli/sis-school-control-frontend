1f1cf69dcd1f8eb27f1b026ce5e4454f3df6878d
Nicolas Vicente Navarrete Clemente <nicosli@gmail.com>
2018-02-28 21:51:06 -0500

    se configura webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,25 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/mixed/app.css');
+mix.copy('resources/assets/css/fontawesome/webfonts', 'public/webfonts');
+
+mix.styles([
+    'public/mixed/app.css',
+    'resources/assets/css/offcanvas.css',
+    'resources/assets/css/custom.css'
+], 'public/css/app.css', './');
+
+mix.scripts([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+    'resources/assets/js/offcanvas.js',
+    'resources/assets/js/funciones.js'
+], 'public/js/app.js', './');
+
+mix.browserSync({
+    open: 'external',
+    host: 'local.gestionarreserva',
+    proxy: 'local.gestionarreserva',
+    port: 8080
+});
